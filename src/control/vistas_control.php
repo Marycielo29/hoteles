c122ec2d25478c47a14c06e96a9464b5b7f8108f
Marycielo29 <marycielomb29@gmail.com>
2025-10-21 09:25:41 -0500

    2.0

--- src/control/vistas_control.php
+++ src/control/vistas_control.php
@@ -8,28 +8,27 @@ class vistasControlador extends vistaModelo
     {
         return require_once "./src/view/plantilla.php";
     }
-   public function obtenerVistaControlador()
+
+     public function obtenerVistaControlador()
     {
-
-
-        if (!isset($_SESSION['sesion_id'])) {
-            if (isset($_GET['views'])) {
-                $ruta = explode("/", $_GET['views']);
-                if ($ruta[0]=="UpdatePassword") {
-                    $respuesta = "UpdatePassword";
-                }else {
-                    $respuesta = "login";
-                }
-            } else {
-                $respuesta = "login";
-            }
-        } else {
         if (isset($_GET['views'])) {
             $ruta = explode("/", $_GET['views']);
             $respuesta = vistaModelo::obtener_vista($ruta[0]);
+            if ($respuesta != "api") {
+                if (!isset($_SESSION['sesion_id'])) {
+                    $respuesta = "login";
+                } else {
+                    $respuesta = vistaModelo::obtener_vista($ruta[0]);
+                }
+            } else {
+                $respuesta = vistaModelo::obtener_vista($ruta[0]);
+            }
         } else {
-            $respuesta = "inicio.php";
-        }
+            if (!isset($_SESSION['sesion_id'])) {
+                $respuesta = "login";
+            } else {
+                $respuesta = "inicio.php";
+            }
         }
         return $respuesta;
     }
