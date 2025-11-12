a51895cfc8dba1a073c25d2b5dd007e1f6b8b72a
Marycielo29 <marycielomb29@gmail.com>
2025-11-12 10:29:43 -0500

    .

--- src/view/js/app.js
+++ src/view/js/app.js
@@ -3,7 +3,7 @@
     baseURL: 'https://reservas.programacion.com.pe/src/control/apiController.php', // Cambiar por tu dominio
     token: '6abe2cda9a65aacb6a8f53f90af4acebfac82cc611b96d7a11733366aa15291e-20251106-9' // Tu token de acceso
 }; */
-const token = '6abe2cda9a65aacb6a8f53f90af4acebfac82cc611b96d7a11733366aa15291e-20251106-9';
+const token = '69df1475dfefa5e87ad70de12423880b4938438dc4e86359f146d5d8aba56736-20251106-6';
 const baseURL = 'https://reservas.programacion.com.pe/src/control/apiController.php?';
 // ===============================
 // 📅 Actualizar fecha y hora
