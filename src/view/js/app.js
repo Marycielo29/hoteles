91c09f86b57cd5b03689fc4774bdfdac162fb9ae
Marycielo29 <marycielomb29@gmail.com>
2025-11-12 10:01:26 -0500

    .

--- src/view/js/app.js
+++ src/view/js/app.js
@@ -1,6 +1,6 @@
 
 const API_CONFIG = {
-    baseURL: 'https://sisreservas.programacion.com.pe/src/control/apiController.php', // Cambiar por tu dominio
+    baseURL: 'https://reservas.programacion.com.pe/src/control/apiController.php', // Cambiar por tu dominio
     token: '6abe2cda9a65aacb6a8f53f90af4acebfac82cc611b96d7a11733366aa15291e-20251106-9' // Tu token de acceso
 };
 // ===============================
